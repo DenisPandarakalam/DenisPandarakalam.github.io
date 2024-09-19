d251e8be19a229a551345bebd3a2eca8bbc5aebc
Denis Panda <dpanda@DPandaPC>
2024-09-18 17:41:04 -0700

    restart

--- js/script.js
+++ js/script.js
@@ -1,3 +1,66 @@
+function rewindLogoAnim(time) {
+
+    if( Math.floor(vid.currentTime/vid.duration*100) % 49 ) {
+
+        console.log(Math.floor(vid.currentTime/vid.duration*100));
+        animationRequestID = requestAnimationFrame(rewindLogoAnim);
+    }
+    else {
+        vid.currentTime = 0;
+        vid.pause();
+    }
+
+}
+
+function toggleDark() {
+    var colors = ['red', 'black', 'pink'],
+        gradient = $('.gradient');
+    
+    console.log(gradient);
+    
+    for(var i = 0; i < colors.length; i++) {
+
+        if(gradient.hasClass(colors[i])) {
+            gradient.removeClass(colors[i]);
+
+            if(i == colors.length-1) i = 0;
+            else i++;
+
+            gradient.addClass(colors[i++]);
+        }
+    }
+}
+
+function hashChange() {
+    var hash = window.location.hash.substring(1);
+    if(!hash) hash = 'home';
+    $('nav a').removeClass('active');
+    $('section:not(#_'+hash+')').removeClass('active').hide();   
+    $('nav a[href$='+hash+']').addClass('active');
+    $('#_'+hash).show().addClass('active');
+}
+
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+    if(hh > 12){
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+      
+     let time = hh + ":" + mm + session;
+  
+    $('#timeinfo').text(time); 
+    var t = setTimeout(function(){ currentTime() }, 1000); 
+  
+}
+
 $(document).ready(() => {
 
     /** TIME INFO */
@@ -42,8 +105,6 @@ $(document).ready(() => {
     }).on('mouseleave touchend', function() {
 
         /** STOPPING ANIMATION */
-        // animationRequestID = window.requestAnimationFrame(rewindLogoAnim);
-        
         $('#logoanimv').removeAttr('loop');
 
         /** STOPPING AUDIO */
@@ -67,72 +128,4 @@ $(document).ready(() => {
     hashChange();
     $(window).on('hashchange', hashChange);
 });
-
-function rewindLogoAnim(time) {
-
-    if( Math.floor(vid.currentTime/vid.duration*100) % 49 ) {
-
-        console.log(Math.floor(vid.currentTime/vid.duration*100));
-        animationRequestID = requestAnimationFrame(rewindLogoAnim);
-    }
-    else {
-        vid.currentTime = 0;
-        vid.pause();
-    }
-
-}
-
-function toggleDark() {
-    var colors = ['red', 'black', 'pink'],
-        gradient = $('.gradient');
-    
-    console.log(gradient);
-    
-    for(var i = 0; i < colors.length; i++) {
-
-        if(gradient.hasClass(colors[i])) {
-            gradient.removeClass(colors[i]);
-
-            if(i == colors.length-1) i = 0;
-            else i++;
-
-            gradient.addClass(colors[i++]);
-        }
-    }
-}
-
-function hashChange() {
-
-    var hash = window.location.hash.substring(1);
-    if(!hash) hash = 'home';
-        
-    $('nav a').removeClass('active');
-    $('section:not(#_'+hash+')').removeClass('active').hide();   
-
-    $('nav a[href$='+hash+']').addClass('active');
-    $('#_'+hash).show().addClass('active');
-}
-
-function currentTime() {
-    let date = new Date(); 
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let ss = date.getSeconds();
-    let session = "AM";
-  
-      
-    if(hh > 12){
-        session = "PM";
-     }
-  
-     hh = (hh < 10) ? "0" + hh : hh;
-     mm = (mm < 10) ? "0" + mm : mm;
-     ss = (ss < 10) ? "0" + ss : ss;
-      
-     let time = hh + ":" + mm + session;
-  
-    $('#timeinfo').text(time); 
-    var t = setTimeout(function(){ currentTime() }, 1000); 
-  
-}
   
